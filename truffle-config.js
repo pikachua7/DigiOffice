8a84a81f0dbd0792ddb12ea2ec4c40815a186c1a
pikachua7 <atharvapaliwal7@gmail.com>
2021-05-09 10:51:02 +0530

    connected to matic

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
@@ -15,13 +18,20 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider(
           privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
         )
       },
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 3
-    }
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
